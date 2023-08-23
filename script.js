13166b1d9c9ef13a41b394f35f8daabc98f1d55d
yasidew <it21440922@my.sliit.lk>
2023-08-23 08:26:17 +0530

    changes made

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 
-    function getRandomColor(){
+    function getRandomColor(){ // function to get random color 
         const letters = "0123456789ABCDEF";
         let color = "#";
-        for(let i =0; i<6 ; i++){
-            color += letters[Math.floor(Math.random() * 16)];
+        for(let i =0; i<6 ; i++){     // loop to get random color
+            color += letters[Math.floor(Math.random() * 16)];  // to get random color from letters 
         }
 
         return color;
